33f96b7d99b242e84bf0940433f85827767e0f48
94ju <janithherath94@gmail.com>
2018-12-03 15:00:00 +0530

    Initial commit

---
+++ ElasticSearch22/ElasticSearch22/Program.cs
@@ -0,0 +1,58 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+
+namespace ElasticSearch22
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Program checkp = new Program();
+            dynamic c=checkp.Connect();
+            checkp.Connect();
+            checkp.CreateMapping(c);
+            checkp.Check(c);
+        }
+        public dynamic Connect() {
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(
+                node
+            //defaultIndex: "my-application"
+            );
+            var client = new ElasticClient(settings);
+            return client;
+        }
+        public  void Check(ElasticClient client)
+        {
+            var person = new Person
+            {
+                Id = "1",
+                Firstname = "Martijn",
+                Lastname = "Laarman"
+            };
+            var index = client.Index<Person>(person, i => i
+                            .Index("myindex2")
+                            .Type("person")
+                            .Refresh(Elasticsearch.Net.Refresh.True));
+        }
+
+        public void CreateMapping(ElasticClient client)
+        {
+            client.CreateIndex("myindex2", c => c
+                .Mappings(ms => ms
+                    .Map<Person>(m => m.AutoMap())
+                )
+            );
+        }
+    }
+
+
+
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
